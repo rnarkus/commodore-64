45c8920b5ec5fdae761c4fb38a419664e60b2931
cassandragoodby <cassandra.goodby@colorado.edu>
2018-02-08 16:42:17 -0700

    javascript done

--- main.js
+++ main.js
@@ -6,15 +6,54 @@ $(document).ready(function(){
     $(document).keypress(function(e){
         if(e.which == 13) {
             var inputText = $('input[name=textInput]').val();
-            if (inputText == "Background"){
+            inputText = inputText.toLowerCase();
+            inputText = inputText.replace(/\s/g, ''); //getting rid of Spaces
+            console.log(inputText);
+            if (inputText == "background"){
+                
                 $("#main .Background").appendTo(".element");
                 $('input[name=textInput]').val("")
             }
-            if (inputText == "Commands"){
+            else if (inputText == "commands"){
 
                 $("#main .Commands").appendTo(".element");
                 $('input[name=textInput]').val("")
             }
+            else if (inputText == "screen"){
+
+                $("#main .Screen").appendTo(".element");
+                $('input[name=textInput]').val("")
+            }
+            else if (inputText == "cassettetapes"){
+
+                $("#main .cassettetapes").appendTo(".element");
+                $('input[name=textInput]').val("")
+            }
+            else if (inputText == "floppydisk"){
+
+                $("#main .floppydisk").appendTo(".element");
+                $('input[name=textInput]').val("")
+            }
+            else if (inputText == "instructions"){
+
+                $("#main .instructions").appendTo(".element");
+                $('input[name=textInput]').val("")
+            }
+            else if (inputText == "waittime"){
+
+                $("#main .waittime").appendTo(".element");
+                $('input[name=textInput]').val("")
+            }
+            else if (inputText == "accessories"){
+
+                $("#main .accessories").appendTo(".element");
+                $('input[name=textInput]').val("")
+            }
+            else{
+                $("#main .error").appendTo(".element");
+                $('input[name=textInput]').val("")
+            }
+
 
         };
     });
