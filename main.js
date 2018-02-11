0499d523b6687758b9bfbcfbeb80ad7fd8c85f3e
Markus <markus_hudobnik@hotmail.com>
2018-02-11 11:25:53 -0700

    added in flexbox for presentation like slides

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
     $("main").hide();
     $(document).keypress(function(e){
         if(e.which == 13) {
+            $(".element").html("");
             var inputText = $('input[name=textInput]').val();
             inputText = inputText.toLowerCase();
             inputText = inputText.replace(/\s/g, ''); //getting rid of Spaces
             $( "#main div" ).each(function() {
-                var name = $(this).attr("class");
+                var name = $(this).attr("id");
                 if ( name == inputText ){
-                    $("#main ."+name).appendTo(".element");
+                    $("#main #"+name).appendTo(".element");
                 };
             });
             $('input[name=textInput]').val("");
