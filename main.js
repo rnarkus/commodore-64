75377f9a199bf827426e81d0e46c87cfed27971a
Markus <markus_hudobnik@hotmail.com>
2018-02-11 11:58:19 -0700

    added in ability to just push enter and it will go to the next 'slide'

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
+    var count=0;
+    var arr= [];
+    var nameAtt;
     $( ".input-form" ).focus();
     $("main").hide();
+    $( "#main .content" ).each(function() {
+        nameAtt = $(this).attr("id");
+        arr.push(nameAtt);
+        console.log(nameAtt);
+    });
     $(document).keypress(function(e){
         if(e.which == 13) {
             $(".element").html("");
             var inputText = $('input[name=textInput]').val();
+            if (inputText == "" && count <= arr.length){ $("#main #"+arr[count]).appendTo(".element"); count++;}
             inputText = inputText.toLowerCase();
             inputText = inputText.replace(/\s/g, ''); //getting rid of Spaces
-            $( "#main div" ).each(function() {
+            $( "#main .content" ).each(function() {
                 var name = $(this).attr("id");
-                if ( name == inputText ){
-                    $("#main #"+name).appendTo(".element");
-                };
+                if ( name == inputText ){$("#main #"+name).appendTo(".element");}
             });
             $('input[name=textInput]').val("");
         };
